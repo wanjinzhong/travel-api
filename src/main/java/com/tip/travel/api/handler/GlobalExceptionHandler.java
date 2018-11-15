2b432640381ba7cc31358aa52e92fa4270aa5010
Neil Wan <neilw@synnex.org>
2018-11-15 10:24:32 +0800

    add exception log

--- src/main/java/com/tip/travel/api/handler/GlobalExceptionHandler.java
+++ src/main/java/com/tip/travel/api/handler/GlobalExceptionHandler.java
@@ -1,24 +1,32 @@
 package com.tip.travel.api.handler;
 
 
+import javax.servlet.http.HttpServletRequest;
+
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.tip.travel.api.utils.JsonEntity;
+import com.tip.travel.common.exception.BizException;
 import com.tip.travel.common.exception.UnauthenticatedException;
+import com.tip.travel.common.service.ExceptionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * GlobalExceptionHandler
+ *
  * @author Neil Wan
  * @create 18-8-4
  */
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @Reference
+    private ExceptionService exceptionService;
+
     private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
 
@@ -31,20 +39,16 @@ public class GlobalExceptionHandler {
         return jsonEntity;
     }
 
-    /**
-     * 捕捉AccountException
-     * @param request
-     * @param e
-     * @return
-     */
-    @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
     public JsonEntity globalException(HttpServletRequest request, Throwable e) {
         logger.error("ERROR: " + e.getMessage(), e);
-        // report(e);
         JsonEntity jsonEntity = new JsonEntity();
         jsonEntity.setStatus(getStatus(request).value());
         jsonEntity.setMessage(e.getMessage());
+        if (!(e instanceof BizException)) {
+            new Thread(() -> exceptionService.logException(e)).start();
+        }
         return jsonEntity;
     }
 
